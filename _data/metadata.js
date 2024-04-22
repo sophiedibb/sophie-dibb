37ab8dcfa7f0c8d40e12bb0bae6bf079facc2e61
Sophie Dibb <sophiedibb1997@gmail.com>
2024-04-23 00:17:12 +0100

    added a contact page and form

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "Sophie Dibb's Blog",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing my first blob post using static site generators",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Sophie Dibb",
+		email: "Sophiedibb1997@gmail.com",
 		url: "https://example.com/about-me/"
 	}
 }
