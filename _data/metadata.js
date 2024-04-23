b8c74b1619415437995f30ea34a231a8efe98235
Sophie Dibb <sophiedibb1997@gmail.com>
2024-04-23 21:21:09 +0100

    navbar added

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Sophie Dibb's Blog",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing my first blob post using static site generators",
+	description: "I am writing my first blob post using static site generators 11ty",
 	author: {
 		name: "Sophie Dibb",
 		email: "Sophiedibb1997@gmail.com",
